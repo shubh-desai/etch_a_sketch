965b9ab27d1ed8bbd53c470d43b32b882e917ab7
Shubh Desai <shubhdesai2023@gmail.com>
2024-07-10 15:29:58 +0530

    Create grid dynamically of the specified dimensions passed by the user via prompt

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const container = document.querySelector("#container");
+const gridSize = prompt("Enter a grid-size (between 1 and 100)");
+const WIDTH = 480;
+const HEIGHT = 480;
+const divHeight = (HEIGHT / gridSize) + "px";
+const containerHeight = (HEIGHT) + "px";
+const containerWidth = (WIDTH) + "px";
+
+container.style.height = containerHeight;
+container.style.width = containerWidth;
 
 const miniContainers = [];
 
-for (let index = 0; index < 16; index += 1) {
+for (let index = 0; index < gridSize; index += 1) {
     
     miniContainers[index] = document.createElement("div");
     miniContainers[index].classList.add("miniContainers");
@@ -12,9 +21,10 @@ for (let index = 0; index < 16; index += 1) {
 
 const divs = [];
 
-for (let index = 0; index < 16 * 16; index += 1) {
+for (let index = 0; index < gridSize * gridSize; index += 1) {
     
     divs[index] = document.createElement("div");
     divs[index].classList.add("divs");    
-    miniContainers[Math.floor(index / 16)].appendChild(divs[index]);
+    miniContainers[Math.floor(index / gridSize)].appendChild(divs[index]);
+    divs[index].style.height = divHeight;
 }
